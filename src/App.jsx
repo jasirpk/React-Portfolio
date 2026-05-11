9ea4016ff822edeb15df5e2fac3c76655528f994
Jasir PK <jasir@tathkarah.in>
2026-05-11 23:17:55 +0530

    configuration setup completed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+const App = () => {
+  return (
+    <div>
+      <h1 class="text-3xl font-bold underline">
+        Hello world!, Iam jasir
+      </h1>
+    </div>
+  )
+}
+
+export default App
