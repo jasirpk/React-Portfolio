8e028cb57fe42064b9ae33d9fa6e38a283891d48
Jasir PK <jasijasu959@gmail.com>
2026-05-16 23:28:18 +0530

    code structured

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,11 +2,11 @@ import linkedin from '../assets/linkedin.png'
 import github from '../assets/github.png'
 
 const Hero = () => {
-    const socialIcons = [
-        {icon: linkedin, alt: 'linkedin'},
-        {icon: github, alt: 'github'},
-    
-    ]
+  const socialIcons = [
+    { icon: linkedin, alt: 'linkedin' },
+    { icon: github, alt: 'github' },
+
+  ]
   return (
     <div>Hero</div>
   )
